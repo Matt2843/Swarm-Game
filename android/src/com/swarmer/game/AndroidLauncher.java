d2bdd0e195ef62241504eec5d27577778959ffa0
Georg Thomassen <geo.cornelius@gmail.com>
2017-02-07 15:36:56 +0100

    initial commit

---
+++ android/src/com/swarmer/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.swarmer.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.swarmer.game.SwarmerMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SwarmerMain(), config);
+	}
+}
