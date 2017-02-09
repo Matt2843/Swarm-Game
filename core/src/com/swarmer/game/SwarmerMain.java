3a078d8cf4f960eec309a1528592b6bef22cb6bb
Matt <s134532@student.dtu.dk>
2017-02-09 18:31:31 +0100

    Added code for viewing tiledmap and moving camera

--- core/src/com/swarmer/game/SwarmerMain.java
+++ core/src/com/swarmer/game/SwarmerMain.java
@@ -2,32 +2,80 @@ package com.swarmer.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 
 public class SwarmerMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	private IsometricTiledMapRenderer renderer;
+	private OrthographicCamera camera;
+	private TiledMap map;
+	
+	private void input(int keyCode) {
+		if(keyCode == Input.Keys.LEFT) {
+			camera.position.x -= 200;
+		}
+		if(keyCode == Input.Keys.RIGHT) {
+			camera.position.x += 200;
+		}
+		if(keyCode == Input.Keys.UP) {
+			camera.position.y += 200;
+		}
+		if(keyCode == Input.Keys.UP) {
+			camera.position.y -= 200;
+		}
+	}
+	
+	
+	@Override
+	public void resize(int width, int height) {
+		camera.viewportWidth = width;
+		camera.viewportHeight = height;
+		camera.update();
+	}
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		map = new TmxMapLoader().load("map.tmx");
+		renderer = new IsometricTiledMapRenderer(map);
+		camera = new OrthographicCamera();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			camera.translate(-10, 0, 0);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			camera.translate(10, 0, 0);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			camera.translate(0, 10, 0);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			camera.translate(0, -10, 0);
+		}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.X)) {
+			camera.rotate(20);
+		}
+		
+		camera.update();
+		
+		renderer.setView(camera);
+		renderer.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		map.dispose();
+		renderer.dispose();
 	}
 }
