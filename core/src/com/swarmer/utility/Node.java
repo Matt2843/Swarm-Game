4f2ffdc637d81f3651d0748ae190b5e8e4118d82
Matt2843 <s134532@student.dtu.dk>
2017-02-14 16:45:08 +0100

    Next node selection WIP in the Node class

--- core/src/com/swarmer/utility/Node.java
+++ core/src/com/swarmer/utility/Node.java
@@ -1,12 +1,24 @@
 package com.swarmer.utility;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 
 public class Node {
 	
 	private final Vector2 position;
+	private Array<Node> nodesInVicinity;
 	
 	private Resource resource = null;
+	
+	public Node nextNode(Node previousNode) {
+		/*
+		 * Implement:
+		 * set coefficient of previousNode low
+		 * roll through nodesInVicinity
+		 * return the node
+		 */
+		return null;
+	}
 
 	public Node(Vector2 position) {
 		this.position = position;
