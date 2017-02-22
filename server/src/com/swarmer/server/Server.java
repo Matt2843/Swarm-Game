aa0ab3d93e3bf75f1063ed2d004ff981d9894807
Matt <s134532@student.dtu.dk>
2017-02-22 17:38:04 +0100

    Server construction WIP

--- server/src/com/swarmer/server/Server.java
+++ server/src/com/swarmer/server/Server.java
@@ -1,5 +1,20 @@
 package com.swarmer.server;
 
-public class Server {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server extends Thread {
+	
+	private ServerSocket server;
+	private Socket connection;
+	private final int port;
+	
+	public Server(int port) {
+		this.port = port;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
 }
