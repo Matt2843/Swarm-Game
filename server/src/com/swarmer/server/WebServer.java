b892df74726c4fac33081d35da15af9f01f0befc
Georg Thomassen <geo.cornelius@gmail.com>
2017-02-23 14:51:11 +0100

    added handlebars

--- server/src/com/swarmer/server/WebServer.java
+++ server/src/com/swarmer/server/WebServer.java
@@ -1,10 +1,31 @@
 package com.swarmer.server;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
-public class WebServer extends HttpServlet
-{
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class WebServer {
+	public static void main(String[] args) {
+
+		get("template-example", new Route() {
+			@Override
+			public Object handle(Request req, Response res) throws Exception {
+				Map<String, Object> model = new HashMap<>();
+				return render(model, "path-to-template");
+			}
+		});
+
+	}
+
+	private static String render(Map<String, Object> model, String templatePath) {
+		return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+	}
 }
