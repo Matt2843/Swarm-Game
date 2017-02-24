56e27ef5e7856a6f6b316d1690ff861eea0e9116
Cap'n Odin <albertjuhl@gmail.com>
2017-02-24 21:54:03 +0100

    Handled exception caused by tiles without edges

--- core/src/com/swarmer/game/SwarmerMain.java
+++ core/src/com/swarmer/game/SwarmerMain.java
@@ -93,14 +93,14 @@ public class SwarmerMain extends ApplicationAdapter implements InputProcessor {
 		centerCamera();
 
 		for (int i = 0; i < 100; i++) {
-			int x = ThreadLocalRandom.current().nextInt(1, 99);
-			int y = ThreadLocalRandom.current().nextInt(1, 99);
+			int x = 50; // ThreadLocalRandom.current().nextInt(1, 99);
+			int y = 50; // ThreadLocalRandom.current().nextInt(1, 99);
 
-			if (graph.nodes[x][y] != null) {
+			if (graph.nodes[x][y] != null && graph.nodes[x][y].getConnectedEdges().size > 0) {
 				ants.add(new Ant(new Sprite(new Texture("player.png")), (TiledMapTileLayer) map.getLayers().get(1), graph.nodes[x][y]));
-			} else {
-				System.out.println("død");
-			}
+			} //else {
+				//System.out.println("død");
+			//}
 		}
 	}
 
