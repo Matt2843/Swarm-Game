c519366d682e61639ecb9d35e970b1266567b7a1
Cap'n Odin <albertjuhl@gmail.com>
2017-02-26 21:30:25 +0100

    Simplified Main

--- core/src/com/swarmer/game/SwarmerMain.java
+++ core/src/com/swarmer/game/SwarmerMain.java
@@ -8,11 +8,14 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.swarmer.utility.Graph;
 
 import java.util.ArrayList;
@@ -23,9 +26,17 @@ public class SwarmerMain extends ApplicationAdapter implements InputProcessor {
 	private IsometricTiledMapRenderer renderer;
 	private OrthographicCamera camera;
 	private TiledMap map;
+
+	private ExtendViewport viewport;
 	
 	private float mapWidth, mapHeight;
 	private int xOffset, yOffset;
+
+	public final static float SCALE = 32f;
+	public final static float INV_SCALE = 20.f/SCALE;
+
+	public final static float VP_WIDTH = 1280 * INV_SCALE;
+	public final static float VP_HEIGHT = 720 * INV_SCALE;
 
 	private ArrayList<Ant> ants = new ArrayList<>();
 	
@@ -73,25 +84,18 @@ public class SwarmerMain extends ApplicationAdapter implements InputProcessor {
 		camera.position.x = mapWidth / 2;
 		camera.position.y = 0;
 	}
-
-	@Override
-	public void resize(int width, int height) {
-		camera.viewportWidth = width;
-		camera.viewportHeight = height;
-		camera.update();
-	}
 	
-	@Override
-	public void create () {
+	@Override public void create () {
 		map = new TmxMapLoader().load("alb.tmx");
 		Graph graph = new Graph(map);
 		renderer = new IsometricTiledMapRenderer(map);
 		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(VP_WIDTH, VP_HEIGHT, camera);
 		Gdx.input.setInputProcessor(this);
 
 		centerCamera();
 
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100; i++) {
 			int x = 50; // ThreadLocalRandom.current().nextInt(1, 99);
 			int y = 50; // ThreadLocalRandom.current().nextInt(1, 99);
 
@@ -101,8 +105,7 @@ public class SwarmerMain extends ApplicationAdapter implements InputProcessor {
 		}
 	}
 
-	@Override
-	public void render() {
+	@Override public void render() {
 		handleInput();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -120,68 +123,62 @@ public class SwarmerMain extends ApplicationAdapter implements InputProcessor {
 		renderer.getBatch().end();
 	}
 	
-	@Override
-	public void dispose () {
-		map.dispose();
-		renderer.dispose();
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
+	Vector3 tp = new Vector3();
+	Vector3 tp2 = new Vector3();
 	boolean dragging;
-	
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		if (button != Input.Buttons.LEFT || pointer > 0) return false;
-        dragging = true;
-        xOffset = screenX;
-        yOffset = screenY;
-        return true;
+	@Override public boolean mouseMoved (int screenX, int screenY) {
+		// we can also handle mouse movement without anything pressed
+		camera.unproject(tp.set(screenX, screenY, 0));
+		return false;
 	}
 
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+	@Override public boolean touchDown (int screenX, int screenY, int pointer, int button) {
+		// ignore if its not left mouse button or first touch pointer
 		if (button != Input.Buttons.LEFT || pointer > 0) return false;
-        dragging = false;
-        return true;
+		camera.unproject(tp.set(screenX, screenY, 0));
+		dragging = true;
+		return true;
 	}
 
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
+	@Override public boolean touchDragged (int screenX, int screenY, int pointer) {
 		if (!dragging) return false;
-		camera.translate(getInBounds((int) (-(screenX - xOffset) * camera.zoom), (int) ((screenY - yOffset) * camera.zoom)));
-		xOffset = screenX;
-		yOffset = screenY;
+		camera.unproject(tp2.set(screenX, screenY, 0));
+		camera.translate(getInBounds((int) (tp.x - tp2.x), (int) (tp.y - tp2.y)));
 		return true;
 	}
 
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
+	@Override public boolean touchUp (int screenX, int screenY, int pointer, int button) {
+		if (button != Input.Buttons.LEFT || pointer > 0) return false;
+		camera.unproject(tp.set(screenX, screenY, 0));
+		dragging = false;
 		return true;
 	}
 
-	@Override
-	public boolean scrolled(int amount) {
+	@Override public void resize (int width, int height) {
+		// viewport must be updated for it to work properly
+		viewport.update(width, height, true);
+	}
+
+	@Override public void dispose () {
+		// disposable stuff must be disposed
+	}
+
+	@Override public boolean keyDown (int keycode) {
+		return false;
+	}
+
+	@Override public boolean keyUp (int keycode) {
+		return false;
+	}
+
+	@Override public boolean keyTyped (char character) {
+		return false;
+	}
+
+	@Override public boolean scrolled(int amount) {
 		if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) && ((amount < 0 && camera.zoom > amount * -1) || (amount > 0 && camera.zoom < 3))) {
 			camera.zoom += amount * 0.2f;
 		}
 		return false;
 	}
-
 }
