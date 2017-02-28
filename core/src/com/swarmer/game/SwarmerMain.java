d46d63a2b9c179bb72a244f7afaa2b2d01e4ede8
Matt2843 <s134532@student.dtu.dk>
2017-02-28 20:00:27 +0100

    Cleaned Swarmer Main

--- core/src/com/swarmer/game/SwarmerMain.java
+++ core/src/com/swarmer/game/SwarmerMain.java
@@ -1,21 +1,14 @@
 package com.swarmer.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.swarmer.screens.ScreenLib;
 import com.swarmer.screens.ScreenManager;
 
 public class SwarmerMain extends Game {
-
-	public SpriteBatch batch;
-	public BitmapFont font;
-
+	
 	public void create() {
 		ScreenManager.getInstance().initialize(this);
 		ScreenManager.getInstance().show(ScreenLib.MAIN_MENU_SCREEN);
-		batch = new SpriteBatch();
-		font = new BitmapFont();
 	}
 
 	public void render() {
@@ -23,7 +16,5 @@ public class SwarmerMain extends Game {
 	}
 
 	public void dispose() {
-		batch.dispose();
-		font.dispose();
 	}
 }
