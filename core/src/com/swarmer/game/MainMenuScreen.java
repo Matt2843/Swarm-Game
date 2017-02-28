111c83b3e9e67b92194353361775090d61473b89
Matt2843 <s134532@student.dtu.dk>
2017-02-28 16:55:46 +0100

    Added MainMenuScreen *WIP*

---
+++ core/src/com/swarmer/game/MainMenuScreen.java
@@ -0,0 +1,59 @@
+package com.swarmer.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+/**
+ * Created by Matt on 02/28/2017.
+ */
+public class MainMenuScreen extends Stage implements Screen {
+
+    private Game g;
+
+    public MainMenuScreen(Game g, int width, int height) {
+        this.g = g;
+        setViewport(new ExtendViewport(width, height));
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        act(delta);
+        draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        dispose();
+    }
+}
