703134deeb5e54e75adec33ddc63022d19dedc69
Matt <s134532@student.dtu.dk>
2017-03-02 16:46:30 +0100

    Created abstract structure class

--- core/src/com/swarmer/game/structures/Structure.java
+++ core/src/com/swarmer/game/structures/Structure.java
@@ -3,5 +3,9 @@ package com.swarmer.game.structures;
 /**
  * Created by Matt on 02-03-2017.
  */
-public class Structure {
+public abstract class Structure {
+
+    /*
+     * TODO: Implement abstract class structure for structures ingame containing spawn methods etc.
+     */
 }
