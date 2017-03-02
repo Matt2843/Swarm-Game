20708c5f067f47c788b26b5269664bf9bbabb9aa
Cap'n Odin <albertjuhl@gmail.com>
2017-03-03 00:41:23 +0100

    Handle index out of bounds in all versions of Spawn

--- core/src/com/swarmer/utility/Temp.java
+++ core/src/com/swarmer/utility/Temp.java
@@ -19,9 +19,11 @@ public class Temp {
 	}
 
 	public static Boolean spawn(Vector2 vec) {
-		if(gameScreen().graph.nodes[(int) vec.x][(int) vec.y] != null && gameScreen().graph.nodes[(int) vec.x][(int) vec.y].getConnectedEdges().size > 0) {
-			gameScreen().getAnts().add(new Ant((TiledMapTileLayer) gameScreen().getMap().getLayers().get(1), gameScreen().graph.nodes[(int) vec.x][(int) vec.y]));
-			return true;
+		if(between((int) vec.x, 0, gameScreen().graph.nodes.length) && between((int) vec.y, 0, gameScreen().graph.nodes[(int) vec.x].length)) {
+			if(gameScreen().graph.nodes[(int) vec.x][(int) vec.y] != null && gameScreen().graph.nodes[(int) vec.x][(int) vec.y].getConnectedEdges().size > 0) {
+				gameScreen().getAnts().add(new Ant((TiledMapTileLayer) gameScreen().getMap().getLayers().get(1), gameScreen().graph.nodes[(int) vec.x][(int) vec.y]));
+				return true;
+			}
 		}
 		return false;
 	}
