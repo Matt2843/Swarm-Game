12063fa1293cc82efc336fc51a99e6d6a17eefd3
CapnOdin <chip-10@hotmail.com>
2017-03-02 17:30:01 +0100

    Inplimented Utility Function Spawn

--- core/src/com/swarmer/utility/Temp.java
+++ core/src/com/swarmer/utility/Temp.java
@@ -5,5 +5,16 @@ package com.swarmer.utility;
  */
 public class Temp {
 
+  public Boolean spawn(String PlayerId, int x, int y){
+      Vector2 vec = getTileCoordinates(x, y);
+    if(gameScreen().graph.nodes[vec.x][vec.y] != null && gameScreen().graph.nodes[vec.x][vec.y].getConnectedEdges().size > 0) {
+      gameScreen().ants.add(new Ant(PlayerId, (TiledMapTileLayer) gameScreen().map.getLayers().get(1), gameScreen().graph.nodes[vec.x][vec.y]));
+      return true;
+    }
+    return false;
+  }
 
+  private GameScreen gameScreen(){
+    return ScreenManager.getInstance().getScreen(com.swarmer.gui.screens.ScreenLib.GAME_SCREEN);
+  }
 }
