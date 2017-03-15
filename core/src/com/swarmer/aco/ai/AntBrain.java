36539cbfdd80159aceaeb9b24ce91dd379572e15
CapnOdin <chip-10@hotmail.com>
2017-03-15 14:51:43 +0100

    Implimented States ("Seek, Return")

--- core/src/com/swarmer/aco/ai/AntBrain.java
+++ core/src/com/swarmer/aco/ai/AntBrain.java
@@ -15,6 +15,8 @@ public class AntBrain {
 	
 	private float c1 = 0.8f;
 	
+	private Boolean bool = false;
+	public String state = "Seek";
 	private Edge previousEdge;
 	private Node currentNode;
 	private Node previousNode;
@@ -25,71 +27,96 @@ public class AntBrain {
 		currentNode = startingNode;
 		previousNode = currentNode;
 	}
-
+	
 	public Edge determineNextPath() {
-		Array<Float> edgeLikelihood = new Array<Float>();
-		
 		double rngesus = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
-
+		
+		Array<Float> edgeLikelihood = getEdgeLikelihood(getTotalPheromones());
+		
+		float accumulated = 0.0f;
+		int decision = 0;
+		
+		for(int i = 0; i < edgeLikelihood.size; i++) {
+			accumulated += edgeLikelihood.get(i);
+			if(rngesus <= accumulated) {
+				decision = i;
+				break;
+			}
+		}
+		
+		if(bool){
+			Edge nextEdge = previousEdge;
+			bool = false;
+		} else {
+			Edge nextEdge = currentNode.getConnectedEdges().get(decision);
+		}
+		
+		previousNode = currentNode;
+		currentNode = nextEdge.getNode();
+		
+		if(currentNode.getResource().getQuantity() > 0) {
+			c1 = 1f;
+			state = "Returning";
+			bool = true;
+		}
+		
+		if(state = "Returning"){
+			nextEdge.getPheromones(PLAYER_ID).addPheromone(100);
+		} else if("Seek") {
+			nextEdge.getPheromones(PLAYER_ID).addPheromone(1);
+		}
+		
+		previousEdge = nextEdge.reverse;
+		return nextEdge;
+	}
+	
+	private int getTotalPheromones() {
 		int totalPheromones = 0;
 		for(Edge evaluationEdge : currentNode.getConnectedEdges()) {
 			if(evaluationEdge != previousEdge) {
 				totalPheromones += evaluationEdge.getPheromones(PLAYER_ID).getQuantity();
 			}
 		}
-
-		float defaultProbability = (float) 1 / (currentNode.getConnectedEdges().size - 1);
-		for(Edge evaluationEdge : currentNode.getConnectedEdges()) {
-
-			if(evaluationEdge != previousEdge) {
-				float pheromone = (float) evaluationEdge.getPheromones(PLAYER_ID).getQuantity();
-				float decision = pheromone / totalPheromones * c1 + defaultProbability * (1 - c1);
-				edgeLikelihood.add(decision);
-			} else {
-				edgeLikelihood.add(0.0f);
-			}
-		}
-		
-		float accumulated = 0.0f;
-		int decision = 0;
-
-		for(int i = 0; i < edgeLikelihood.size; i++) {
-			float chance = (float) edgeLikelihood.get(i);
-			accumulated += chance;
-			if(rngesus <= accumulated) {
-				decision = i;
-				break;
-			}
-		}
-
-		Edge nextEdge = currentNode.getConnectedEdges().get(decision);
-
-		nextEdge.getPheromones(PLAYER_ID).addPheromone();
-
-		previousNode = currentNode;
-		currentNode = nextEdge.getNode();
-		previousEdge = nextEdge.reverse;
-		return nextEdge;
+		return totalPheromones;
 	}
-
+	
+	private Array<Float> getEdgeLikelihood(int totalPheromones) {
+		Array<Float> edgeLikelihood = new Array<Float>();
+		float defaultProbability = (float) 1 / (currentNode.getConnectedEdges().size - 1);
+			for(Edge evaluationEdge : currentNode.getConnectedEdges()) {
+				if(evaluationEdge != previousEdge) {
+					if(totalPheromones > 0) {
+						float pheromone = (float) evaluationEdge.getPheromones(PLAYER_ID).getQuantity();
+						float decision = pheromone / totalPheromones * c1 + defaultProbability * (1 - c1);
+					} else {
+						decision = defaultProbability;
+					}
+					edgeLikelihood.add(decision);
+				} else {
+					edgeLikelihood.add(0.0f);
+				}
+			}
+		return edgeLikelihood;
+	}
+	
 	public Edge getPreviousEdge() {
 		return previousEdge;
 	}
-
+	
 	public Node getPreviousNode() {
 		return previousNode;
 	}
-
+	
 	public void setPreviousEdge(Edge previousEdge) {
 		this.previousEdge = previousEdge;
 	}
-
+	
 	public Node getCurrentNode() {
 		return currentNode;
 	}
-
+	
 	public void setCurrentNode(Node currentNode) {
 		this.currentNode = currentNode;
 	}
-
+	
 }
