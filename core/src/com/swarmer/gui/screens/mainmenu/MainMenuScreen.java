b7cba848ce503201107b92a201a63211351e49ef
Matt2843 <s134532@student.dtu.dk>
2017-03-28 23:33:36 +0200

    Login Screen WIP

--- core/src/com/swarmer/gui/screens/mainmenu/MainMenuScreen.java
+++ core/src/com/swarmer/gui/screens/mainmenu/MainMenuScreen.java
@@ -1,9 +1,12 @@
 package com.swarmer.gui.screens.mainmenu;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -11,16 +14,18 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.swarmer.game.SwarmerMain;
+import com.swarmer.gui.screens.ScreenLib;
 import com.swarmer.gui.screens.ScreenManager;
-import com.swarmer.gui.widgets.BubbleWidget;
 
 public class MainMenuScreen extends Stage implements Screen {
+
+	private Texture backgroundTexture;
+	private Sprite backgroundSprite;
+	private SpriteBatch spriteBatch;
 
 	private final SwarmerMain game;
 
 	private Group buttonGroup;
-
-	private TextButton[] buttons = new TextButton[5];
 
 	public MainMenuScreen(final SwarmerMain game, int width, int height) {
 		this.game = game;
@@ -28,42 +33,33 @@ public class MainMenuScreen extends Stage implements Screen {
 		create();
 	}
 
+	private void loadBackground() {
+		spriteBatch = new SpriteBatch();
+		backgroundTexture = new Texture(Gdx.files.internal("swarmer-v1.png"));
+		backgroundSprite = new Sprite(backgroundTexture);
+	}
+
+	private void handleInput() {
+		if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+			ScreenManager.getInstance().show(ScreenLib.GAME_SCREEN);
+		}
+	}
+
 	private void create() {
+		loadBackground();
 		buttonGroup = new Group();
-
-		for(int i = 0; i < buttons.length; i++) {
-			buttons[i] = new TextButton("Button " + (i + 1), new MainMenuButtonSkin());
-			final int finalI = i;
-			buttons[i].addCaptureListener(new ChangeListener() {
-				@Override public void changed(ChangeEvent event, Actor actor) {
-					System.out.println("Button " + (finalI + 1) + " pressed.");
-				}
-			});
-		}
-		for(int i = 0; i < buttons.length; i++) {
-			if(i > 0) {
-				buttons[i].setPosition(0, buttons[i - 1].getY() - buttons[i].getHeight());
-			} else {
-				buttons[i].setPosition(0, 0);
-			}
-			buttonGroup.addActor(buttons[i]);
-		}
-
-		buttons[0].setText("Play");
-		buttons[0].addCaptureListener(new ChangeListener() {
+		TextButton loginButton = new TextButton("Login", new MainMenuButtonSkin());
+		loginButton.addCaptureListener(new ChangeListener() {
 			@Override public void changed(ChangeEvent event, Actor actor) {
-				ScreenManager.getInstance().show(com.swarmer.gui.screens.ScreenLib.GAME_SCREEN);
+				System.out.println("Hello World");
 			}
 		});
 
-		buttons[4].setText("Exit");
-		buttons[4].addCaptureListener(new ChangeListener() {
-			@Override public void changed(ChangeEvent event, Actor actor) {
-				Gdx.app.exit();
-			}
-		});
+		buttonGroup.addActor(loginButton);
 
-		buttonGroup.setPosition(getWidth() / 2 - buttons[0].getWidth() / 2, getHeight() / 2 + 5 * buttons[0].getHeight() / 2 - buttons[0].getHeight());
+		buttonGroup.setOrigin(loginButton.getWidth() / 2, loginButton.getHeight() / 2);
+		buttonGroup.setPosition(Gdx.graphics.getWidth() / 2 - (loginButton.getWidth() / 2), Gdx.graphics.getHeight() / 5 - (loginButton.getHeight() / 2));
+
 		addActor(buttonGroup);
 	}
 
@@ -72,8 +68,13 @@ public class MainMenuScreen extends Stage implements Screen {
 	}
 
 	@Override public void render(float delta) {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		handleInput();
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		spriteBatch.begin();
+			spriteBatch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		spriteBatch.end();
+
 		act(delta);
 		draw();
 	}
