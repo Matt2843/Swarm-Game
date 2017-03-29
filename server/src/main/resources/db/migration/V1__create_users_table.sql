f8a1edd53e79b9c2c9c98b08b5780dbe35c57c81
Georg Thomassen <geo.cornelius@gmail.com>
2017-03-29 21:27:20 +0200

    added salt to migration

--- server/src/main/resources/db/migration/V1__create_users_table.sql
+++ server/src/main/resources/db/migration/V1__create_users_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE users (
     id VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    password_salt VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 );
