31351be372a3d1ad0e759e3fb03c092c36f705b9
Matt2843 <s134532@student.dtu.dk>
2017-04-05 16:59:19 +0200

    WIP Lobby

--- shared/src/com/swarmer/shared/communication/Player.java
+++ shared/src/com/swarmer/shared/communication/Player.java
@@ -1,52 +1,7 @@
 package com.swarmer.shared.communication;
 
-import java.io.Serializable;
-
 /**
- * Created by Matt on 03/16/2017.
+ * Created by Matt on 04/05/2017.
  */
-public final class Player implements Serializable {
-
-    private String alias;
-    private int id;
-
-    public Player(String alias, int id) {
-        this.alias = alias;
-        this.id = id;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override public boolean equals(Object player) {
-        if (this == player) return true;
-        if (player == null || getClass() != player.getClass()) return false;
-
-        Player that = (Player) player;
-
-        return id == that.id && (alias != null ? player.equals(that.getAlias()) : that.getAlias() == null);
-    }
-
-    @Override public int hashCode() {
-        int result = alias != null ? alias.hashCode() : 0;
-        result = 31 * result + id;
-        return result;
-    }
-
-    @Override public String toString() {
-        return "p" + alias + "i" + id;
-    }
+public class Player {
 }
