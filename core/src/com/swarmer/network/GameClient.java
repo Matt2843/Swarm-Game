c5c69a9e73f95675ff0235161d968c31620341c5
Matt <s134532@student.dtu.dk>
2017-04-06 10:39:30 +0200

    Android emulator internet access granted

--- core/src/com/swarmer/network/GameClient.java
+++ core/src/com/swarmer/network/GameClient.java
@@ -19,7 +19,7 @@ public final class GameClient extends Thread {
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
 	
-	private String host = "localhost";
+	private String host = "10.16.137.101";
 	private int port = 1111;
 	
 	private Socket client;
