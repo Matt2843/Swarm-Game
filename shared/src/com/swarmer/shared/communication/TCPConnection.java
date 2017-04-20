7b7f32abfe18265dcb2e001e4f119e046578f522
Georg Thomassen <geo.cornelius@gmail.com>
2017-04-20 23:05:58 +0200

    bugfix

--- shared/src/com/swarmer/shared/communication/TCPConnection.java
+++ shared/src/com/swarmer/shared/communication/TCPConnection.java
@@ -49,7 +49,11 @@ public class TCPConnection extends Connection {
 
 	public void stopConnection(Object... o) {
 		try {
-			sendMessage(new Message(0, o));
+			if (o.length > 0) {
+				sendMessage(new Message(0, o[0]));
+			} else {
+				sendMessage(new Message(0));
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
