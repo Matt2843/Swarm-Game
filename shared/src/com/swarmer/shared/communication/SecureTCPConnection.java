d9c16c3e178c14eff64dbd77e2553411437a5308
CapnOdin <chip-10@hotmail.com>
2017-04-21 10:05:56 +0200

    Hotfix

--- shared/src/com/swarmer/shared/communication/SecureTCPConnection.java
+++ shared/src/com/swarmer/shared/communication/SecureTCPConnection.java
@@ -162,10 +162,9 @@ public class SecureTCPConnection extends Connection {
 		try {
 			ByteArrayInputStream bis = new ByteArrayInputStream(data);
 			byte[] bytes = new byte[200];
-			//while(bis.read(bytes) > 0) {
-			//	lst.add(new SealedObject(bytes, outCipher));
-			//}
-			lst.add(new SealedObject(data, outCipher));
+			while(bis.read(bytes, 0, 200) > 0) {
+				lst.add(new SealedObject(bytes, outCipher));
+			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		} catch(IllegalBlockSizeException e) {
