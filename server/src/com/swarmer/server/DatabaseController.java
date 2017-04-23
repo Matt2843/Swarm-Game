e2ee1eed598c2556821861c2eab36f079d915720
Cap'n Odin <albertjuhl@gmail.com>
2017-04-24 00:16:34 +0200

    Added a set of encryption keys

--- server/src/com/swarmer/server/DatabaseController.java
+++ server/src/com/swarmer/server/DatabaseController.java
@@ -7,6 +7,9 @@ import com.swarmer.shared.communication.TCPConnection;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 
 public class DatabaseController {
@@ -14,16 +17,28 @@ public class DatabaseController {
 	public static MySQLConnection mySQLConnection = new MySQLConnection("localhost", 3306);
 	private final DatabaseControllerProtocol mothershipProtocol = new DatabaseControllerProtocol();
 
+	public static KeyPair KEY = null;
 	private ServerSocket serverSocket;
 	private Socket connection;
 	private final int port;
 
 	public DatabaseController(int port) {
 		this.port = port;
+		generateKeys();
 		clearDatabase();
 		try {
 			awaitConnection();
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void generateKeys() {
+		try {
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+			kpg.initialize(2048);
+			KEY = kpg.generateKeyPair();
+		} catch(NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 	}
