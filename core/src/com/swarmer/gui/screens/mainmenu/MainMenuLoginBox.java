ab85fbe56cbd9cb7bdea2e5dc497b6305631257f
Cap'n Odin <albertjuhl@gmail.com>
2017-04-24 20:08:12 +0200

    Massages are only send if textFieldData have been initialised

--- core/src/com/swarmer/gui/screens/mainmenu/MainMenuLoginBox.java
+++ core/src/com/swarmer/gui/screens/mainmenu/MainMenuLoginBox.java
@@ -16,118 +16,117 @@ import java.io.IOException;
  */
 public class MainMenuLoginBox extends Table {
 
-    private TextField userName;
-    private TextField password;
-    private TextField verifyPassword;
+	private TextField userName;
+	private TextField password;
+	private TextField verifyPassword;
 
-    private Label userNameLabel;
-    private Label passwordLabel;
-    private Label verifyPasswordLabel;
+	private Label userNameLabel;
+	private Label passwordLabel;
+	private Label verifyPasswordLabel;
 
-    private TextButton login;
-    private TextButton createUser;
+	private TextButton login;
+	private TextButton createUser;
 
-    private Skin defaultSkin;
+	private Skin defaultSkin;
 
-    private FileHandle uiskin = Gdx.files.internal("default/skin/uiskin.json");
+	private FileHandle uiskin = Gdx.files.internal("default/skin/uiskin.json");
 
-    public MainMenuLoginBox() {
-        createFields();
-    }
+	public MainMenuLoginBox() {
+		createFields();
+	}
 
-    private void createFields() {
-        defaultSkin = new Skin(uiskin);
+	private void createFields() {
+		defaultSkin = new Skin(uiskin);
 
-        userName = new TextField("", defaultSkin);
-        userNameLabel = new Label("Account Name: ", defaultSkin);
+		userName = new TextField("", defaultSkin);
+		userNameLabel = new Label("Account Name: ", defaultSkin);
 
-        password = new TextField("", defaultSkin);
-        passwordLabel = new Label("Password: ", defaultSkin);
-        password.setPasswordCharacter('*');
-        password.setPasswordMode(true);
+		password = new TextField("", defaultSkin);
+		passwordLabel = new Label("Password: ", defaultSkin);
+		password.setPasswordCharacter('*');
+		password.setPasswordMode(true);
 
-        verifyPassword = new TextField("", defaultSkin);
-        verifyPasswordLabel = new Label("Verify Password: ", defaultSkin);
-        verifyPassword.setPasswordCharacter('*');
-        verifyPassword.setPasswordMode(true);
+		verifyPassword = new TextField("", defaultSkin);
+		verifyPasswordLabel = new Label("Verify Password: ", defaultSkin);
+		verifyPassword.setPasswordCharacter('*');
+		verifyPassword.setPasswordMode(true);
 
-        verifyPassword.setVisible(false);
-        verifyPasswordLabel.setVisible(false);
+		verifyPassword.setVisible(false);
+		verifyPasswordLabel.setVisible(false);
 
-        login = new TextButton("Login", defaultSkin);
-        createUser = new TextButton("Create User", defaultSkin);
+		login = new TextButton("Login", defaultSkin);
+		createUser = new TextButton("Create User", defaultSkin);
 
-        login.addCaptureListener(new ChangeListener() {
-            @Override public void changed(ChangeEvent event, Actor actor) {
-                Object[] textFieldData = null;
-                try {
-                    if(userName.getText().matches("^[a-zA-Z0-9][a-zA-Z0-9_\\-']{1,28}[a-zA-Z0-9]$")) {
-                        textFieldData = new Object[]{userName.getText(), password.getText().toCharArray()};
-                    } else {
-                        // TODO: Inform user that he's slightly retarded
-                    }
-                    if(login.getText().toString().equals("Create")) {
-                        if(password.getText().equals(verifyPassword.getText())) {
-                            GameClient.getInstance().stcp.sendMessage(new Message(201, textFieldData));
-                        }
-                    } else {
-                        GameClient.getInstance().stcp.sendMessage(new Message(109, textFieldData));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                loginState();
-                clearFields();
-            }
-        });
+		login.addCaptureListener(new ChangeListener() {
+			@Override public void changed(ChangeEvent event, Actor actor) {
+				Object[] textFieldData = null;
+				try {
+					if(userName.getText().matches("^[a-zA-Z0-9][a-zA-Z0-9_\\-']{1,28}[a-zA-Z0-9]$")) {
+						textFieldData = new Object[]{userName.getText(), password.getText().toCharArray()};
+						if(login.getText().toString().equals("Create")) {
+							if(password.getText().equals(verifyPassword.getText())) {
+								GameClient.getInstance().stcp.sendMessage(new Message(201, textFieldData));
+							}
+						} else {
+							GameClient.getInstance().stcp.sendMessage(new Message(109, textFieldData));
+						}
+					} else {
+						// TODO: Inform user that he's slightly retarded
+					}
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+				loginState();
+				clearFields();
+			}
+		});
 
-        createUser.addCaptureListener(new ChangeListener() {
-            @Override public void changed(ChangeEvent event, Actor actor) {
-                if(createUser.getText().toString().equals("Create User")) {
-                    createUserState();
-                } else {
-                    loginState();
-                }
+		createUser.addCaptureListener(new ChangeListener() {
+			@Override public void changed(ChangeEvent event, Actor actor) {
+			if(createUser.getText().toString().equals("Create User")) {
+				createUserState();
+			} else {
+				loginState();
+			}
+			}
+		});
 
-            }
-        });
+		defaults().width(125);
+		add(userNameLabel);
+		add(userName).width(225);
+		row();
+		add(passwordLabel);
+		add(password).width(225);
+		row();
+		add(verifyPasswordLabel);
+		add(verifyPassword).width(225);
+		row();
+		add(login).colspan(2).width(350);
+		row();
+		add(createUser).colspan(2).width(350);
 
-        defaults().width(125);
-        add(userNameLabel);
-        add(userName).width(225);
-        row();
-        add(passwordLabel);
-        add(password).width(225);
-        row();
-        add(verifyPasswordLabel);
-        add(verifyPassword).width(225);
-        row();
-        add(login).colspan(2).width(350);
-        row();
-        add(createUser).colspan(2).width(350);
+		setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 5);
+	}
 
-        setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 5);
-    }
+	private void createUserState() {
+		verifyPassword.setVisible(true);
+		verifyPasswordLabel.setVisible(true);
+		login.setText("Create");
+		createUser.setText("Cancel");
+	}
 
-    private void createUserState() {
-        verifyPassword.setVisible(true);
-        verifyPasswordLabel.setVisible(true);
-        login.setText("Create");
-        createUser.setText("Cancel");
-    }
+	private void loginState() {
+		verifyPassword.setVisible(false);
+		verifyPasswordLabel.setVisible(false);
+		createUser.setText("Create User");
+		login.setText("Login");
+	}
 
-    private void loginState() {
-        verifyPassword.setVisible(false);
-        verifyPasswordLabel.setVisible(false);
-        createUser.setText("Create User");
-        login.setText("Login");
-    }
-
-    private void clearFields() {
-        userName.setText("");
-        verifyPassword.setText("");
-        password.setText("");
-    }
+	private void clearFields() {
+		userName.setText("");
+		verifyPassword.setText("");
+		password.setText("");
+	}
 
 
 }
