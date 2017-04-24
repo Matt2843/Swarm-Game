d382239713766ee7763daadc8115c5435f59e862
Cap'n Odin <albertjuhl@gmail.com>
2017-04-24 19:07:34 +0200

    Changed to support the new version of its superclass

--- server/src/com/swarmer/server/DatabaseControllerCallable.java
+++ server/src/com/swarmer/server/DatabaseControllerCallable.java
@@ -4,15 +4,13 @@ import com.swarmer.server.units.ServerUnit;
 import com.swarmer.shared.communication.Callable;
 import com.swarmer.shared.communication.IPGetter;
 import com.swarmer.shared.communication.Message;
+import com.swarmer.shared.communication.TCPConnection;
 
 import java.io.IOException;
 import java.net.Socket;
 
-/**
- * Created by Matt on 04/12/2017.
- */
 public class DatabaseControllerCallable extends Callable {
 	public DatabaseControllerCallable(Message message) throws IOException {
-		super(new Socket(IPGetter.getInstance().getDatabaseControllerIP(), ServerUnit.DATABASE_CONTROLLER_TCP_PORT), message);
+		super(new TCPConnection(new Socket(IPGetter.getInstance().getDatabaseControllerIP(), ServerUnit.DATABASE_CONTROLLER_TCP_PORT), null), message);
 	}
 }
