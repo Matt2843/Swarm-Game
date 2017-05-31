20f4842c895b24b14cbd5cbc26eb93692fbc3f5d
Georg Thomassen <geo.cornelius@gmail.com>
2017-05-31 12:21:53 +0200

    wip

--- core/src/com/swarmer/network/ClientProtocol.java
+++ core/src/com/swarmer/network/ClientProtocol.java
@@ -94,7 +94,7 @@ public class ClientProtocol extends Protocol {
 
 	private void userJoinedLobby(Message message) {
 		Player joinedPlayer = (Player) message.getObject();
-		LobbyScreen.getInstance().getFindGame().remove();
+//		LobbyScreen.getInstance().getFindGame().remove();
         LobbyUserList.getInstance().addPlayer(joinedPlayer);
 		LobbyUserList.getInstance().addUserToList(joinedPlayer.getUsername());
 		SwarmerMain.getInstance().show(LobbyScreen.getInstance());
